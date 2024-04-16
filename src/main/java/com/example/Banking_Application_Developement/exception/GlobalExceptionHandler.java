e0c6a34ad4f191f653ec97e80806bb2eafe958a6
Rufai Sanni <sruffy07@gmail.com>
2024-04-16 12:20:27 +0100

    exception handler class update

--- src/main/java/com/example/Banking_Application_Developement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/Banking_Application_Developement/exception/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,15 +14,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public String handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
-        return e.getMessage();
-    }
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public String handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+//        return e.getMessage();
+//    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(DataIntegrityViolationException.class)
@@ -40,19 +45,19 @@ public class GlobalExceptionHandler {
         return e.getMessage();
     }
 
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
-//        Map<String, String> errorMap = new HashMap<>();
-////        exception.getBindingResult().getFieldErrors().forEach(error -> errorMap.put(error.getField(), error.getDefaultMessage()));
-//
-//        BindingResult bindingResult = exception.getBindingResult();
-//        List<FieldError> errors = bindingResult.getFieldErrors();
-//        for(FieldError error: errors){
-//            errorMap.put(error.getField(), error.getDefaultMessage());
-//        }
-//        return errorMap;
-//    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        Map<String, String> errorMap = new HashMap<>();
+//        exception.getBindingResult().getFieldErrors().forEach(error -> errorMap.put(error.getField(), error.getDefaultMessage()));
+
+        BindingResult bindingResult = exception.getBindingResult();
+        List<FieldError> errors = bindingResult.getFieldErrors();
+        for(FieldError error: errors){
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        }
+        return errorMap;
+    }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(NoSuchElementException.class)
